d405d0138ca6eeb1d77695f5aed50901c2010026
Dmytri Baglai <dmytri.baglai@wonga.com>
2013-08-28 14:52:13 +0100

    Horizontal wheel shadow adjusted

--- library/src/antistatic/spinnerwheel/WheelHorizontalView.java
+++ library/src/antistatic/spinnerwheel/WheelHorizontalView.java
@@ -111,9 +111,6 @@ public class WheelHorizontalView extends AbstractWheelView {
 
     @Override
     public void setSelectorPaintCoeff(float coeff) {
-        if (mItemsDimmedAlpha >= 100)
-            return;
-
         LinearGradient shader;
 
         int w = getMeasuredWidth();
@@ -141,7 +138,7 @@ public class WheelHorizontalView extends AbstractWheelView {
             int c2 = Math.round( c2f ) << 24;
             int c3 = Math.round( c3f ) << 24;
 
-            int[] colors = { c2, c2, c2, c2, 0xff000000, 0xff000000, c2, c2, c2, c2 };
+            int[] colors = { c1, c1, c1, c1, 0xff000000, 0xff000000, c1, c1, c1, c1 };
             float[] positions = { 0, p3, p3, p1, p1, p2, p2, p4, p4, 1 };
             shader = new LinearGradient(0, 0, w, 0, colors, positions, Shader.TileMode.CLAMP);
         }
